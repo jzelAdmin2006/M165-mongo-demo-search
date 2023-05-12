40921e12bc1cba3faa44e133a947f2788c86e19b
Johannes Zeller <johannes_zeller@sluz.ch>
2023-05-12 10:24:03 +0200

    solve task 3

--- main.py
+++ main.py
@@ -5,11 +5,28 @@ from pymongo import MongoClient
 connectionString = "mongodb://localhost:27017/"
 client = MongoClient(connectionString)
 db = client["db_restaurant"]
-collection = db["restaurants"]
+restaurants = db["restaurants"]
 print("Alle Stadtbezirke:")
-for borough in collection.distinct("borough"):
+for borough in restaurants.distinct("borough"):
     print("- " + borough)
 
 print("Top 3 restaurants mit höchstem Rating:")
-for restaurant in collection.find().sort("grades.0.score", -1).limit(3):
+for restaurant in restaurants.find().sort("grades.0.score", -1).limit(3):
     print("- " + restaurant["name"] + " (Rating " + str(restaurant["grades"][0]["score"]) + ")")
+
+le_perigord = restaurants.find_one({"name": "Le Perigord"})
+print("Le Perigord:")
+le_perigord_coord = le_perigord["address"]["coord"]
+print(le_perigord_coord)
+
+print("Restaurant, welches am nächsten zu Le Perigord liegt (Pythagoras):")
+nearest_restaurant = None
+nearest_restaurant_distance = None
+for restaurant in restaurants.find({"$and": [{"address.coord": {"$ne": []}}, {"name": {"$ne": "Le Perigord"}}]}):
+    restaurant_coord = restaurant["address"]["coord"]
+    restaurant_distance = ((restaurant_coord[0] - le_perigord_coord[0]) ** 2 + (
+            restaurant_coord[1] - le_perigord_coord[1]) ** 2) ** 0.5
+    if nearest_restaurant is None or restaurant_distance < nearest_restaurant_distance:
+        nearest_restaurant = restaurant
+        nearest_restaurant_distance = restaurant_distance
+print("- " + nearest_restaurant["name"] + " (" + str(nearest_restaurant_distance) + ")")
