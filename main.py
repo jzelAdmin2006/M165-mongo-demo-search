7f37a51b8231e6cbe7a16877ac2d037c2c6ce943
Johannes Zeller <johannes_zeller@sluz.ch>
2023-05-12 10:28:15 +0200

    solve task 4

--- main.py
+++ main.py
@@ -15,11 +15,11 @@ for restaurant in restaurants.find().sort("grades.0.score", -1).limit(3):
     print("- " + restaurant["name"] + " (Rating " + str(restaurant["grades"][0]["score"]) + ")")
 
 le_perigord = restaurants.find_one({"name": "Le Perigord"})
-print("Le Perigord:")
+print("Le Perigord Koordinaten:")
 le_perigord_coord = le_perigord["address"]["coord"]
 print(le_perigord_coord)
 
-print("Restaurant, welches am nächsten zu Le Perigord liegt (Pythagoras):")
+print("Restaurant, welches am nächsten zu Le Perigord liegt:")
 nearest_restaurant = None
 nearest_restaurant_distance = None
 for restaurant in restaurants.find({"$and": [{"address.coord": {"$ne": []}}, {"name": {"$ne": "Le Perigord"}}]}):
@@ -29,4 +29,21 @@ for restaurant in restaurants.find({"$and": [{"address.coord": {"$ne": []}}, {"n
     if nearest_restaurant is None or restaurant_distance < nearest_restaurant_distance:
         nearest_restaurant = restaurant
         nearest_restaurant_distance = restaurant_distance
-print("- " + nearest_restaurant["name"] + " (" + str(nearest_restaurant_distance) + ")")
+print("- " + nearest_restaurant["name"] + " (Distanz " + str(nearest_restaurant_distance) + ")")
+
+le_perigord_cuisine = le_perigord["cuisine"]
+print("Le Perigord Küche:")
+print(le_perigord_cuisine)
+
+print("Restaurant, welches am nächsten zu Le Perigord liegt und die gleiche Küche hat:")
+nearest_restaurant = None
+nearest_restaurant_distance = None
+for restaurant in restaurants.find({"$and": [{"address.coord": {"$ne": []}}, {"name": {"$ne": "Le Perigord"}},
+                                             {"cuisine": le_perigord_cuisine}]}):
+    restaurant_coord = restaurant["address"]["coord"]
+    restaurant_distance = ((restaurant_coord[0] - le_perigord_coord[0]) ** 2 + (
+            restaurant_coord[1] - le_perigord_coord[1]) ** 2) ** 0.5
+    if nearest_restaurant is None or restaurant_distance < nearest_restaurant_distance:
+        nearest_restaurant = restaurant
+        nearest_restaurant_distance = restaurant_distance
+print("- " + nearest_restaurant["name"] + " (Distanz " + str(nearest_restaurant_distance) + ")")
